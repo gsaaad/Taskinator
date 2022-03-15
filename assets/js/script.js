22e7b2c63e9e46aa4313371709ad278f1f0916f5
George Saad <gsparqcode@gmail.com>
2022-03-15 00:29:02 -0400

    Added form and button functionality for adding tasks and preventing default behavior of browser

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
 //access the element usng QuerySelector
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 //access tasks using QuerySelector
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //when button is clicked, create element, give it class task-item, text context and append to listItem
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+  event.preventDefault();
   var listeItemEl = document.createElement("li");
   listeItemEl.className = "task-item";
   listeItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listeItemEl);
 };
 //using the variable for button, add event listener for "click", run the createTaskHandler function
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
