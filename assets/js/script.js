19e5bfcb67a1d007df7e2a5d57c1d260b8053508
George Saad <gsparqcode@gmail.com>
2022-03-15 01:08:37 -0400

    Finalized drop down menu for task type input

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,32 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 //when button is clicked, create element, give it class task-item, text context and append to listItem
 var createTaskHandler = function (event) {
   event.preventDefault();
+  var taskNameInput = document.querySelector("input[name = 'task-name']").value;
+  var taskTypeInput = document.querySelector(
+    "select[name = 'task-type']"
+  ).value;
+  console.log(taskTypeInput);
+
+  //create list item
   var listeItemEl = document.createElement("li");
   listeItemEl.className = "task-item";
-  listeItemEl.textContent = "This is a new task.";
+
+  //create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+
+  //give div a class name
+  taskInfoEl.className = "task-info";
+
+  //add HTML content to the div
+  taskInfoEl.innerHTML =
+    "<h3 class='task-name'>" +
+    taskNameInput +
+    "</h3><span class='task-type'>" +
+    taskTypeInput +
+    "</span>";
+  listeItemEl.appendChild(taskInfoEl);
+
+  //add entire list item to list
   tasksToDoEl.appendChild(listeItemEl);
 };
 //using the variable for button, add event listener for "click", run the createTaskHandler function
